6787909732734d848bc8119ee0633097e98a1367
yassminaganga <gangayassmina@gmail.com>
2025-09-17 13:10:02 +0100

    chapitre

--- script.js
+++ script.js
@@ -9,12 +9,18 @@
 //   let somme = addition(8, 5);
 //   console.log("La somme est : " + somme);
   
-let age = 20;
+// let age = 20;
 
-if (age < 18) {
-  console.log("Vous êtes mineur.");
-} else if (age === 18) {
-  console.log("Vous venez d’atteindre la majorité !");
-} else {
-  console.log("Vous êtes majeur.");
-}
+// if (age < 18) {
+//   console.log("Vous êtes mineur.");
+// } else if (age === 18) {
+//   console.log("Vous venez d’atteindre la majorité !");
+// } else {
+//   console.log("Vous êtes majeur.");
+// }
+
+for (let i = 1; i <= 10; i++) {
+    if (i === 5) continue; // sauter le 5
+    console.log("Nombre : " + i);
+  }
+  
